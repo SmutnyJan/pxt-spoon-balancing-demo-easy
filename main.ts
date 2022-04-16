d7e13229258136604e4d11b602527f2bdbd7c07c
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 16:08:24 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+balancing.onEggDrop(function () {
+    if (jeHraSpustena) {
+        jeHraSpustena = false
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    jeHraSpustena = !(jeHraSpustena)
+})
+let jeHraSpustena = false
+balancing.setDifficulty(Difficulty.Easy)
+basic.forever(function () {
+    if (jeHraSpustena) {
+        balancing.tick()
+    }
+})
